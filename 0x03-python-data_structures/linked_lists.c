9036ee244d0e86d4334198ed44c1689e2ec19225
amusukwa <amusukwa@gmail.com>
2023-08-13 06:42:57 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/linked_lists.c
+++ 0x03-python-data_structures/linked_lists.c
@@ -63,40 +63,19 @@ current->next = new;
 return (new);
 
 }
-
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+void free_listint(listint_t *head)
 {
-listint_t *new;
 listint_t *current;
 
-current = *head;
-
-
-new = malloc(sizeof(listint_t));
-
-if (new == NULL)
-return (NULL);
-
-new->n = n;
-new->next = NULL;
-
-if (*head == NULL)
-*head = new;
-
-else
+while (head != NULL)
 {
-
-while (current->next != NULL)
-
-current = current->next;
-current->next = new;
+current = head;
+head = head->next;
+free(current);
 }
-
-return (new);
 }
