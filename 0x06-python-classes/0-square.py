e754f9e458609f6ef7ebaf451c5408efc38ded62
amusukwa <amusukwa@gmail.com>
2023-08-29 08:33:09 -0700

    0-main.py 2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+"""
+Square module
+
+This module defines the Square class.
+"""
+
 class Square:
     """
     Empty class representing a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
