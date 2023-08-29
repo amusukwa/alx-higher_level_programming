d9e09cfb7b8589f4e5d967096d674a722d57c99a
amusukwa <amusukwa@gmail.com>
2023-08-29 05:10:01 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class representing a square.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        None
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Returns:
+            None
+        """
+        self.__size = size
