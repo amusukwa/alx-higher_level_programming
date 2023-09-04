56bbaddf9e317c8ba4fb6b68f1003baceac2ec86
amusukwa <amusukwa@gmail.com>
2023-09-04 04:34:26 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Defines a rectangle
+
+"""
+class Rectangle:
+    """ This empty class represents a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+This directory contains solutions for the ALX 0x08. Python - More Classes and Objects
+project
