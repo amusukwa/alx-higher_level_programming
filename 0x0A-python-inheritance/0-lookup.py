20403851cd78c897953af136b0871b024f171ba7
amusukwa <amusukwa@gmail.com>
2023-09-11 03:49:41 -0700

    README.md

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" lists attributes of a boject"""
+
+
+def lookup(obj):
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+This directory contains solutions for 0x0A. Python - Inheritance
+project
