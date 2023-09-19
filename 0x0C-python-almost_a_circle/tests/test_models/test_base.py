c18799070f5ddce47fa807133c776c4a1f6f7791
amusukwa <amusukwa@gmail.com>
2023-09-19 03:50:15 -0700

    test corrections

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,6 +13,17 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
 
+    def test_to_json_string(self):
+        class Temp(Base):
+            def to_dictionary(self):
+                return {"id": self.id}
+
+        b1 = Temp(1)
+        b2 = Temp(2)
+        json_str = Temp.to_json_string([b1.to_dictionary(), b2.to_dictionary()])
+        expected = '[{"id": 1}, {"id": 2}]'
+        self.assertEqual(json_str, expected)
+
     # Add more test cases for other methods as needed
 
 if __name__ == '__main__':
