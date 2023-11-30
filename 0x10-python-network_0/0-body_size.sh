cab90d378f155702cf747d97b4c17c14be6ea4cf
amusukwa <amusukwa@gmail.com>
2023-11-30 06:29:14 -0800

    0-body_size.sh 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
-curl -sI "$1" | grep -i content-length | awk '{print "Response size: "$2" bytes"}' | tr -d '\r'
+curl -sI "$1" | grep -i content-length | awk '{ "$2"}' | tr -d '\r'
