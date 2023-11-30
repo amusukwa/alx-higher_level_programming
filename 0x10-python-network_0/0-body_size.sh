924bf9748b83fcead9b3b3b8d3769daa7edc7706
amusukwa <amusukwa@gmail.com>
2023-11-30 09:54:41 -0800

    0-body_size.sh error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
-curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r'
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
+[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1; curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r'
