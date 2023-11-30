511317bbb2193247491ac1cd231f6916e006bd5a
amusukwa <amusukwa@gmail.com>
2023-11-30 06:46:46 -0800

    0-body_size.sh 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
-curl -sI "$1" | grep -i content-length | awk '{ "$2"}' | tr -d '\r'
+curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r'
