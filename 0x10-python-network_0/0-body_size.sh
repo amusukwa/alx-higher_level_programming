15e5e730164b50902cc035156192d862cfee7ae9
amusukwa <amusukwa@gmail.com>
2023-11-30 06:26:03 -0800

    0-body_size.sh 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the provided URL and display the size of the response body
-response_size=$(curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$response_size" ]; then
-    echo "Failed to retrieve response size."
-else
-    echo  "${response_size}"
-fi
+[ -z "$1" ] && echo "Usage: $0 <URL>" && exit 1
+curl -sI "$1" | grep -i content-length | awk '{print "Response size: "$2" bytes"}' | tr -d '\r'
