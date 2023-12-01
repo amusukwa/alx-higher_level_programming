70b3781f717db6a92ee3dfb3297ae616d52d055c
amusukwa <amusukwa@gmail.com>
2023-12-01 02:14:28 -0800

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using urllib.
+"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content.decode("utf-8")))
